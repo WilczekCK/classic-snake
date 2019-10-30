af2e933cc9131747cfedd455df0c37dae1b8cebf
Paweł Wilk <wilczek@macbook-pro-pawe.home>
2019-10-30 15:07:41 +0100

    disabled reverse direction, little refactor

--- game.js
+++ game.js
@@ -6,21 +6,31 @@ settings = {
         height: 575
     },
     allowedKeys: ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'],
-
     gameplay: {
-        scoreCounter : {},
-        drawScoreCounter: function() {
-            this.scoreCounter.clearRect(0, settings.gameContainerSize.height, settings.gameContainerSize.width, settings.gameContainerSize.height+25)
+        scoreCounter: {},
+        drawScoreCounter: function () {
+            this.scoreCounter.clearRect(0, settings.gameContainerSize.height, settings.gameContainerSize.width, settings.gameContainerSize.height + 25)
             this.scoreCounter.font = '30px Roboto';
             this.scoreCounter.fillStyle = 'black';
-            this.scoreCounter.fillText('SCORE: '+settings.snake.stats.points, settings.gameContainerSize.width-795, settings.gameContainerSize.height+20)
+            this.scoreCounter.fillText('SCORE: ' + settings.snake.stats.points, settings.gameContainerSize.width - 795, settings.gameContainerSize.height + 20)
+        },
+        changeDirections: function (key) {
+            if (key == 'ArrowLeft' && settings.snake.actualDirection == 'ArrowRight'
+                || key == 'ArrowRight' && settings.snake.actualDirection == 'ArrowLeft') {
+                return 0
+            } else if (key == 'ArrowUp' && settings.snake.actualDirection == 'ArrowDown'
+                || key == 'ArrowDown' && settings.snake.actualDirection == 'ArrowUp') {
+                return 0;
+            }
+
+            settings.snake.actualDirection = key;
         },
         edgeOfScreen: {
-            horizontal: function(){
-                if(settings.snake.position.x >= settings.gameContainerSize.width || settings.snake.position.x == -settings.snake.stats.width){
-                    switch(settings.snake.position.x){
+            horizontal: function () {
+                if (settings.snake.position.x >= settings.gameContainerSize.width || settings.snake.position.x == -settings.snake.stats.width) {
+                    switch (settings.snake.position.x) {
                         case settings.gameContainerSize.width:
-                            settings.snake.position.x = -25;
+                            settings.snake.position.x = 0;
                             break;
                         case -25:
                             settings.snake.position.x = settings.gameContainerSize.width;
@@ -28,53 +38,60 @@ settings = {
                     }
                 }
             },
-            vertical: function(){
-                if(settings.snake.position.y >= settings.gameContainerSize.height || settings.snake.position.y == -settings.snake.stats.height){
-                    switch(settings.snake.position.y){
-                        case -25:
-                            settings.snake.position.y = settings.gameContainerSize.height+25;
+            vertical: function () {
+                if (settings.snake.position.y >= settings.gameContainerSize.height || settings.snake.position.y == -settings.snake.stats.height) {
+                    switch (settings.snake.position.y) {
+                        case -settings.snake.stats.height:
+                            settings.snake.position.y = settings.gameContainerSize.height + 25;
                             break;
-                        case settings.gameContainerSize.height+25:
+                        case settings.gameContainerSize.height + 25:
                             settings.snake.position.y = 0;
                             break;
                     }
                 }
-            }
-        }
+            },
+            check: function(){
+                this.horizontal();
+                this.vertical();
+            },
+        },
     },
 
     snake: {
         actualDirection: undefined,
-        player : {},
+        player: {},
         stats: {
-            points: 0,
+            points: 1,
             height: 25,
             width: 25
         },
-        position:{
+        position: {
             x: 0,
             y: 0
         },
-        draw:function(){
+        draw: function () {
             this.player.beginPath();
             this.player.rect(this.position.x, this.position.y, this.stats.width, this.stats.height)
             this.player.closePath();
             this.player.fill();
+        },
+        clear: function(){
+            settings.snake.player.clearRect(this.position.x, this.position.y, this.stats.width, this.stats.height)
         },
     },
 
     tail: {
         object: {},
         positions: [],
-        lastSnakePosition:function(){
-            if(settings.snake){
+        lastSnakePosition: function () {
+            if (settings.snake) {
                 this.positions.unshift([settings.snake.position.x, settings.snake.position.y])
                 this.positions.length = settings.snake.stats.points;
                 return [settings.snake.position.x, settings.snake.position.y]
             }
         },
-        draw: function(){
-            if(settings.snake.stats.points > 0){
+        draw: function () {
+            if (settings.snake.stats.points > 1) {
                 this.clear();
                 this.object.beginPath();
                 this.object.rect(this.lastSnakePosition()[0], this.lastSnakePosition()[1], settings.snake.stats.width, settings.snake.stats.height)
@@ -82,16 +99,26 @@ settings = {
                 this.object.fill();
             }
         },
-        clear: function(){
-            if(this.positions[this.positions.length-1]){
-                this.object.clearRect(this.positions[this.positions.length-1][0], this.positions[this.positions.length-1][1], settings.snake.stats.height, settings.snake.stats.width)
+        clear: function () {
+            var lastTabEl = this.positions[this.positions.length - 1]
+            if (lastTabEl) {
+                //0 is x
+                //1 is y
+                this.object.clearRect(lastTabEl[0], lastTabEl[1], settings.snake.stats.height, settings.snake.stats.width)
+            }
+        },
+        collision: function (){
+            const simplified = [settings.snake.actualPosition.x, settings.snake.actualPosition.y]
+            if(settings.tail.positions.includes(simplified)){
+                console.log('yup')
+                return new Error();
             }
         }
     },
-    
+
     score: {
         pixel: {},
-        pixelPoint: -1,
+        pixelPoint: 0,
         actualPosition: {
             x: {},
             y: {}
@@ -100,32 +127,32 @@ settings = {
             height: 5,
             width: 5,
         },
-        rollPosition: function(){
-            var width = Math.floor(Math.random() * (settings.gameContainerSize.width - 0)) + 0;
-            var height = Math.floor(Math.random() * (settings.gameContainerSize.height - 0)) + 0;
+        rollPosition: function () {
+            const width = Math.floor(Math.random() * (settings.gameContainerSize.width - 0)) + 0;
+            const height = Math.floor(Math.random() * (settings.gameContainerSize.height - 0)) + 0;
 
             return [width, height]
         },
-        checkIfScored: function(){
-            if(this.actualPosition.x < settings.snake.position.x + settings.snake.stats.width &&
+        checkIfScored: function () {
+            if (this.actualPosition.x < settings.snake.position.x + settings.snake.stats.width &&
                 this.actualPosition.x + this.size.width > settings.snake.position.x &&
                 this.actualPosition.y < settings.snake.position.y + settings.snake.stats.height &&
-                this.actualPosition.y + this.size.height > settings.snake.position.y){
-                    
+                this.actualPosition.y + this.size.height > settings.snake.position.y) {
+
                 this.goal();
                 this.clear();
                 this.draw();
-            }  
+            }
         },
-        goal: function() {
+        goal: function () {
             settings.snake.stats.points += 1
             settings.gameplay.drawScoreCounter();
         },
-        clear: function() {
-            this.pixel.clearRect(this.actualPosition.x-2, this.actualPosition.y-2, this.size.width+4, this.size.height+4)
+        clear: function () {
+            this.pixel.clearRect(this.actualPosition.x - 2, this.actualPosition.y - 2, this.size.width + 4, this.size.height + 4)
         },
-        draw: function() {
-            if(this.pixelPoint == settings.snake.stats.points) return 0;
+        draw: function () {
+            if (this.pixelPoint == settings.snake.stats.points) return 0;
 
             this.pixelPoint++;
             const roll = {
@@ -140,49 +167,51 @@ settings = {
             this.pixel.rect(roll.x, roll.y, this.size.height, this.size.width)
             this.pixel.closePath();
             this.pixel.stroke();
+        },
+        init: function() {
+            settings.score.draw();
+            settings.score.checkIfScored();
         }
     },
 
-    init: function(){
+    init: function () {
         const fps = 120;
-        setTimeout(function(){
-
+        setTimeout(function () {
             const canvas = settings.gameContainer;
             settings.snake.player = canvas.getContext("2d");
             settings.score.pixel = canvas.getContext("2d");
             settings.tail.object = canvas.getContext("2d");
             settings.gameplay.scoreCounter = canvas.getContext("2d");
-            
-            settings.score.draw();
-            settings.gameplay.drawScoreCounter();
 
-                if(settings.snake.actualDirection){
-                    settings.score.checkIfScored();
-                    settings.gameplay.edgeOfScreen.horizontal();
-                    settings.gameplay.edgeOfScreen.vertical();
-                    settings.snake.player.clearRect(settings.snake.position.x, settings.snake.position.y, settings.snake.stats.width, settings.snake.stats.height)
+            if (settings.snake.actualDirection) {
+                settings.score.init();
 
-                    settings.tail.draw();
-                    switch(settings.snake.actualDirection){
-                        case 'ArrowLeft':
-                            settings.snake.position.x -= 25;
-                            break;
-                        case 'ArrowRight':
-                            settings.snake.position.x += 25;
-                            break;
-                        case 'ArrowUp':
-                            settings.snake.position.y -= 25;
-                            break;
-                        case 'ArrowDown':
-                            settings.snake.position.y += 25;
-                            break;
-                    }
+                settings.gameplay.drawScoreCounter();
+                settings.gameplay.edgeOfScreen.check();
 
-                    settings.snake.draw();
+                settings.snake.clear();                
+                settings.tail.draw();
+
+                switch (settings.snake.actualDirection) {
+                    case 'ArrowLeft':
+                        settings.snake.position.x -= settings.snake.stats.height;
+                        break;
+                    case 'ArrowRight':
+                        settings.snake.position.x += settings.snake.stats.height;
+                        break;
+                    case 'ArrowUp':
+                        settings.snake.position.y -= settings.snake.stats.height;
+                        break;
+                    case 'ArrowDown':
+                        settings.snake.position.y += settings.snake.stats.height;
+                        break;
                 }
 
-                requestAnimationFrame(settings.init)
-                            
+                settings.snake.draw();
+            }
+
+            requestAnimationFrame(settings.init)
+
         }, 10000 / fps)
     }
 }
@@ -190,9 +219,9 @@ settings = {
 window.addEventListener('DOMContentLoaded', _ => {
     requestAnimationFrame(settings.init)
 
-    document.addEventListener('keydown', function ({key}) {
-        if(settings.allowedKeys.includes(key)){
-            settings.snake.actualDirection = key;
+    document.addEventListener('keydown', function ({ key }) {
+        if (settings.allowedKeys.includes(key)) {
+            settings.gameplay.changeDirections(key)
         }
     })
 });
